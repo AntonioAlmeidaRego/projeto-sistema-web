c5d142fbfd7f18877f177eb935b5629811ead9a5
Antonio Almeida <antonio.alm_pdf@hotmail.com>
2020-06-13 22:07:44 -0300

    Primeiro commit

---
+++ SistemaWebApp/src/main/java/br/com/sistemawebapp/SistemaWebAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistemawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaWebAppApplication.class, args);
+	}
+
+}
---
+++ SistemaWebApp/src/test/java/br/com/sistemawebapp/SistemaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
