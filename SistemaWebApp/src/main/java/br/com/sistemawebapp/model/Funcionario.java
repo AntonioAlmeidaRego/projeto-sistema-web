73fa04c2fcfaf6d00800ccceff26ba40d8da4226
Antonio Almeida <antonio.alm_pdf@hotmail.com>
2020-06-15 21:25:09 -0300

    Adicionando entidade Funcionario.java

---
+++ SistemaWebApp/src/main/java/br/com/sistemawebapp/model/Funcionario.java
@@ -0,0 +1,103 @@
+package br.com.sistemawebapp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionario_tb")
+public class Funcionario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * Anotação @Id responsável por definir a chave primaria da tabela. E esse
+	 * atributo deverá ser do tipo Long ou Integer
+	 */
+
+	/*
+	 * Anotação @GeneratedValue(strategy = GenerationType.Auto) responsável por auto
+	 * incrementar o atributo que contenha a chave primaria.
+	 */
+
+	/*
+	 * Anotação @Column responsável por definir informações sobre a coluna da
+	 * tabela. Tais, como o nome, tamanho de caracteres, se a coluna por padrão é
+	 * nula e entre outros
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "nome_cl", nullable = false, length = 40)
+	private String nome;
+	@Column(name = "cpf_cl", nullable = false, length = 20)
+	private String cpf;
+	@Column(name = "rg_cl", nullable = false, length = 20)
+	private String rg;
+	@Column(name = "telefone_cl", nullable = false, length = 20)
+	private String telefone;
+	@Column(name = "numero_carteira_trabalho_cl", nullable = false, length = 30)
+	private String numeroCarteiraTrabalho;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNumeroCarteiraTrabalho() {
+		return numeroCarteiraTrabalho;
+	}
+
+	public void setNumeroCarteiraTrabalho(String numeroCarteiraTrabalho) {
+		this.numeroCarteiraTrabalho = numeroCarteiraTrabalho;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
