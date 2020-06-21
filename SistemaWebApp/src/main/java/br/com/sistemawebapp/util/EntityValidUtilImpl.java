ed50d5b5dc5a84dba6043af8ad3e35db6370ef43
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2020-06-21 17:00:08 -0300

    Novos comentários adicionados...

--- SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityValidUtilImpl.java
+++ SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityValidUtilImpl.java
@@ -8,7 +8,10 @@ import java.util.List;
 import br.com.sistemawebapp.annotations.AttrUnique;
 
 public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUtil<Entity> {
-
+	/*
+	 * Método privado para retornar uma lista de field (atributos da entidade
+	 * passado por parâmetro) e um objeto genérico por parâmetro.
+	 */
 	private List<Field> getFields(Entity entity, Object class1) {
 		AnnotationUtil<?> annotationUtil = new AnnotationUtil<>();
 		List<Field> fields = new ArrayList<Field>();
@@ -21,6 +24,11 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 		return fields;
 	}
 
+	/*
+	 * Método privado para verificar se a entidade genérica possui anotação
+	 * “EntityQuery”. Se for verdadeiro mostra-se que o objeto será realizado
+	 * consulta na base de dados.
+	 */
 	private boolean isClassAnntotation(Entity entity) {
 		for (Annotation annotation : entity.getClass().getAnnotations()) {
 			if (annotation.annotationType().getSimpleName().equals("EntityQuery")) {
@@ -31,9 +39,14 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 		return false;
 	}
 
+	/*
+	 * Método privado para verificar se a entidade genérica possui anotação nos
+	 * atributos “AttrUpdate”. Se for verdadeiro mostra-se que os atributos serão
+	 * verificados se houveram alterações.
+	 */
 	private boolean isFieldAnntotationUpdate(Entity entity) {
 		for (Field field : entity.getClass().getDeclaredFields()) {
-			for(Annotation annotation : field.getAnnotations()) {
+			for (Annotation annotation : field.getAnnotations()) {
 				if (annotation.annotationType().getSimpleName().equals("AttrUpdate")) {
 					return true;
 				}
@@ -42,10 +55,15 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 
 		return false;
 	}
-	
+
+	/*
+	 * Método privado para verificar se a entidade genérica possui anotação no
+	 * atributo com a chave primária “IdUpdate”. Se for verdadeiro mostra-se que o
+	 * atributo será verificado se houve alteração.
+	 */
 	private boolean isFieldAnntotationIdUpdate(Entity entity) {
 		for (Field field : entity.getClass().getDeclaredFields()) {
-			for(Annotation annotation : field.getAnnotations()) {
+			for (Annotation annotation : field.getAnnotations()) {
 				if (annotation.annotationType().getSimpleName().equals("IdUpdate")) {
 					return true;
 				}
@@ -55,51 +73,59 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 		return false;
 	}
 
+	/*
+	 * Método usado para verificar se a entidade atualizou seus atributos. Possui
+	 * três parâmetros, tais como um objeto genérico e dois objetos que serão
+	 * passados na interface genérica.
+	 */
 	@Override
 	public boolean isEntityUpdate(Object object, Entity entity, Entity entityQuery) {
-		List<Integer> list = new ArrayList<Integer>();
-		if (isClassAnntotation(entity)) {
+		Integer increment = 0; // usar uma variavel para incremento. Para verificar a quantidade de atributos na classe
+		if (isClassAnntotation(entity)) { // Verificar se a entidade possui anotação ("EntityQuery")
 			for (Field field : getFields(entity, object)) {
-				field.setAccessible(true);
+				field.setAccessible(true); // acessar atributo
 				try {
 
-					if (field.get(entity) != null) {
+					if (field.get(entity) != null) { // verifica se o atributo possui um valor diferente de null
 
-						String value = field.get(entity).toString();
-						if (!value.equals("")) {
-							if (isFieldAnntotationUpdate(entityQuery)) {
-								String valueUp = field.get(entityQuery).toString();
-								if (!value.equals(valueUp)) {
-									list.add(0);
+						String value = field.get(entity).toString(); // retornar o valor do atributo
+						if (!value.equals("")) { // verifica se o valor não está vazio
+							if (isFieldAnntotationUpdate(entityQuery)) { // verifica se o atributo possui
+																			// anotação("AttrUpdate")
+								String valueUp = field.get(entityQuery).toString(); // retornar o valor vindo da entidade na consulta do banco
+								if (!value.equals(valueUp)) { // verifica se o houve alteração
+									increment++; // se for verdadeiro incrementa a variavel
 								}
 							}
 						}
 					}
 				} catch (NullPointerException e) {
+					increment = -1;
 					e.printStackTrace();
 				} catch (IllegalArgumentException e) {
+					increment = -1;
 					e.printStackTrace();
 				} catch (IllegalAccessException e) {
+					increment = -1;
 					e.printStackTrace();
 				}
 
 			}
 		}
 
-		if (list.size() == getFields(entity, object).size()) {
+		if (increment == getFields(entity, object).size()) {
 			return true;
 		}
 
 		return false;
 	}
-	
+
 	@Override
 	public boolean isIdUpdate(Object object, Entity entity) {
 		if (isClassAnntotation(entity)) {
 			for (Field field : getFields(entity, object)) {
 				boolean existAnn = isFieldAnntotationIdUpdate(entity);
-				System.out.println(existAnn);
-				if(existAnn) {
+				if (existAnn) {
 					field.setAccessible(true);
 					try {
 
@@ -117,7 +143,7 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 						e.printStackTrace();
 					}
 				}
-					
+
 			}
 		}
 
