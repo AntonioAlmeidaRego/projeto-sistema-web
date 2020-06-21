631080d4077e834d28e102de935e3ecdb9a2ccb7
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2020-06-21 19:53:14 -0300

    Finalizando os comentarios na EntityQueryDB e EntityQueryDBImpl

--- SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityQueryDB.java
+++ SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityQueryDB.java
@@ -1,10 +1,33 @@
 package br.com.sistemawebapp.util;
 
 public interface EntityQueryDB<Entity extends Object> {
+	// Método usado para setar o objeto que fará a consulta na base de dados
 	public void setObject(Object object);
+
+	// Método usado para setar o objeto que será retornado na consulta
 	public void setReturnObject(Object object);
+
+	// Método usado para retornar o objeto que foi consultado
 	public Object getReturnObject();
-	public boolean isExist(Object...objects);
+
+	/*
+	 * Método usado para verificar se existe uma entidade com os mesmos dados
+	 * passado por parâmetro. Esse método possui uma possibilidade de infinitos
+	 * parâmetros
+	 */
+	public boolean isExist(Object... objects);
+
+	/*
+	 * Método usado para verificar se a entidade atualizou seus atributos que são
+	 * únicos. Esse método possui três parâmetros, tais como objeto genérico, e dois
+	 * objetos genéricos vindo da interface
+	 */
 	public boolean isUpdate(Object object, Entity entity, Entity entityQuery);
+
+	/*
+	 * Método usado para verificar se o atributo primário da entidade atualizou.
+	 * Esse método possui dois parâmetros, tais como objeto genérico, e um objeto
+	 * genérico vindo da interface
+	 */
 	public boolean isIdUpdate(Object object, Entity entity);
 }
--- SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityValidUtilImpl.java
+++ SistemaWebApp/src/main/java/br/com/sistemawebapp/util/EntityValidUtilImpl.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 import br.com.sistemawebapp.annotations.AttrUnique;
 
+class ReturnFunction {
+	public boolean isReturn = false;
+	public String value = "";
+}
+
 public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUtil<Entity> {
 	/*
 	 * Método privado para retornar uma lista de field (atributos da entidade
@@ -73,6 +78,25 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 		return false;
 	}
 
+	private ReturnFunction isFieldNotEmpty(Field field, Entity entity) {
+		field.setAccessible(true); // acessar atributo
+		ReturnFunction returnFunction = new ReturnFunction();
+		try {
+			if (field.get(entity) != null) { // verifica se o atributo possui um valor diferente de null
+				String value = field.get(entity).toString();// retornar o valor do atributo
+				if (!value.equals("")) { // verifica se o atributo não é vazio
+					returnFunction.isReturn = true;
+					returnFunction.value = value;
+					return returnFunction;
+				}
+			}
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return returnFunction;
+	}
+
 	/*
 	 * Método usado para verificar se a entidade atualizou seus atributos. Possui
 	 * três parâmetros, tais como um objeto genérico e dois objetos que serão
@@ -80,68 +104,54 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 	 */
 	@Override
 	public boolean isEntityUpdate(Object object, Entity entity, Entity entityQuery) {
-		Integer increment = 0; // usar uma variavel para incremento. Para verificar a quantidade de atributos na classe
+		Integer increment = 0; // usar uma variavel para incremento. Para verificar a quantidade de atributos
+								// na classe
 		if (isClassAnntotation(entity)) { // Verificar se a entidade possui anotação ("EntityQuery")
 			for (Field field : getFields(entity, object)) {
-				field.setAccessible(true); // acessar atributo
-				try {
-
-					if (field.get(entity) != null) { // verifica se o atributo possui um valor diferente de null
-
-						String value = field.get(entity).toString(); // retornar o valor do atributo
-						if (!value.equals("")) { // verifica se o valor não está vazio
-							if (isFieldAnntotationUpdate(entityQuery)) { // verifica se o atributo possui
-																			// anotação("AttrUpdate")
-								String valueUp = field.get(entityQuery).toString(); // retornar o valor vindo da entidade na consulta do banco
-								if (!value.equals(valueUp)) { // verifica se o houve alteração
-									increment++; // se for verdadeiro incrementa a variavel
-								}
+				final boolean isNotEmpty = isFieldNotEmpty(field, entityQuery).isReturn;
+				final String value = isFieldNotEmpty(field, entityQuery).value;
+				if (isNotEmpty) {
+					if (isFieldAnntotationUpdate(entityQuery)) { // verifica se o atributo possui
+																	// anotação("AttrUpdate")
+						String valueUp;
+						try {
+							valueUp = field.get(entityQuery).toString(); // retornar o valor vindo da
+																			// entidade na consulta do banco
+							if (!value.equals(valueUp)) { // verifica se o houve alteração
+								increment++; // se for verdadeiro incrementa a variavel
 							}
+						} catch (IllegalArgumentException | IllegalAccessException e) {
+							increment = -1;
+							e.printStackTrace();
 						}
+
 					}
-				} catch (NullPointerException e) {
-					increment = -1;
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					increment = -1;
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					increment = -1;
-					e.printStackTrace();
 				}
 
 			}
 		}
 
-		if (increment == getFields(entity, object).size()) {
+		if (increment == getFields(entity, object).size()) {// Verifica se o tamanho de atributos com anotação e que
+															// seja de acordo com a regra acima, é igual ao número de
+															// atributos com anotação
 			return true;
 		}
 
 		return false;
 	}
-
+	/*
+	 * Método usado para verificar se a chave primaria da entidade foi atualizada. Possui
+	 * dois parâmetros, tais como um objeto genérico e um objeto que será
+	 * passado na interface genérica.
+	 */
 	@Override
 	public boolean isIdUpdate(Object object, Entity entity) {
-		if (isClassAnntotation(entity)) {
+		if (isClassAnntotation(entity)) {// Verificar se a entidade possui anotação ("EntityQuery")
 			for (Field field : getFields(entity, object)) {
 				boolean existAnn = isFieldAnntotationIdUpdate(entity);
-				if (existAnn) {
-					field.setAccessible(true);
-					try {
-
-						if (field.get(entity) != null) {
-							String value = field.get(entity).toString();
-							if (!value.equals("")) {
-								return true;
-							}
-						}
-					} catch (NullPointerException e) {
-						e.printStackTrace();
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					}
+				if (existAnn) { // Verificar se o atributo possui anotação ("IdUpdate")
+					// Se o retorno da função for verdadeiro retorna true, caso contrario false
+					return isFieldNotEmpty(field, entity).isReturn == true ? true : false;
 				}
 
 			}
@@ -152,30 +162,19 @@ public class EntityValidUtilImpl<Entity extends Object> implements EntityValidUt
 
 	@Override
 	public boolean isEntityValid(Object object, Entity entity, Entity entityQuery) {
-		List<Integer> list = new ArrayList<Integer>();
-		if (isClassAnntotation(entity)) {
+		Integer increment = 0;// usar uma variavel para incremento. Para verificar a quantidade de atributos
+								// na classe
+		if (isClassAnntotation(entity)) {// Verificar se a entidade possui anotação ("EntityQuery")
 			for (Field field : getFields(entity, object)) {
-				field.setAccessible(true);
-				try {
-
-					if (field.get(entity) != null) {
-						String value = field.get(entity).toString();
-						if (!value.equals("")) {
-							list.add(0);
-						}
-					}
-				} catch (NullPointerException e) {
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
+				if (isFieldNotEmpty(field, entityQuery).isReturn) { // Verificar se o atributo não é vazio e nem nulo
+					increment++;
 				}
-
 			}
 		}
 
-		if (list.size() == getFields(entity, object).size()) {
+		if (increment == getFields(entity, object).size()) {// Verifica se o tamanho de atributos com anotação e que
+															// seja de acordo com a regra acima, é igual ao número de
+															// atributos com anotação
 			return true;
 		}
 
